411731290098a90bc855e5194a275fcfd3fac482
ssebide <sebiddejohn1362gmail.com>
2025-11-06 13:16:38 +0300

    Integrate and test ActixWeb

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use email_newsletter::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // Create the listener first
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+
+    // Pass it to run()
+    run(listener)?.await
 }
