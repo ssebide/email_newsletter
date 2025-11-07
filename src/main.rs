dfb624fcc7006559a2ef28a2bfdb86b354c451d5
ssebide <sebiddejohn136@gmail.com>
2025-11-07 12:42:32 +0300

    Test different endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use email_newsletter::run;
 use std::net::TcpListener;
 
+use actix_web::web;
+#[derive(serde::Deserialize)]
+struct FormData {
+    username: String,
+}
+/// Extract form data using serde.
+/// This handler get called only if content type is *x-www-form-urlencoded*
+/// and content of the request could be deserialized to a `FormData` struct
+fn index(form: web::Form<FormData>) -> String {
+    format!("Welcome {}!", form.username)
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Create the listener first
